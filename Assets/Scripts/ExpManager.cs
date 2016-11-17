da82b4c4a078784fd49737c86ff3715000b3a8b7
ckromero <ckromero@gmail.com>
2016-11-16 23:50:07 -0500

    exp manager ready for testing

--- Assets/Scripts/ExpManager.cs
+++ Assets/Scripts/ExpManager.cs
@@ -11,9 +11,36 @@ public class ExpManager : MonoBehaviour
 
 	public AudioManager audioManager;
 	public LevelsManager levelsManager;
+	private TriggerListener triggerListener = new TriggerListener();
+	private float lastTriggerTime;
+
+	void Start () { 
+	}
 
 
 	void Update() {
+		if (triggerListener.LastDoublePress != Mathf.Infinity && triggerListener.LastDoublePress > lastTriggerTime) {
+			lastTriggerTime = triggerListener.LastDoublePress;
+			//TODO: AWFUL
+			if (expState == ExpStates.Idle)			
+				IsIntroScreen = true;
+			if (expState == ExpStates.Intro)			
+				IsInstructionScreen = true;
+			if (expState == ExpStates.Instructions)			
+				IsCurtainOpen = true;
+			if (expState == ExpStates.CurtainOpen)			
+				IsLevels = true;
+			if (expState == ExpStates.Levels)			
+				IsCurtainClose = true;
+			if (expState == ExpStates.CurtainClose)			
+				IsOutro = true;
+			if (expState == ExpStates.Outro)			
+				IsCredits = true;
+			if (expState == ExpStates.Credits)			
+				IsIdle = true;
+
+		}
+
 //		states = new bool[]{ IsIdle, IsIntroScreen, IsInstructionScreen, IsCurtainOpen, IsLevels, IsCurtainClose, IsOutro, IsTitles };
 		if (IsIdle) { 
 			expState = ExpStates.Idle;
